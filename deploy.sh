ac9209a02ca0f583835e2052cafd11f2fdf3cd2b
Shaziya Nusrath <sksn2202@gmail.com>
2023-05-12 07:13:18 +0000

    Updated

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 This file contains deployment related code
-
+Again i am changing to do second commit
+and i will see how it works.
---
+++ test.sh
@@ -0,0 +1,6 @@
+Just created to test second commit
+As i am learning how to do commits 
+how see logs
+how to see status 
+and how to push to repo
+

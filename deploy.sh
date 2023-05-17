2bf95de3722ae37a88ab05427d09342ef1b5019b
Shaziya Nusrath <sksn2202@gmail.com>
2023-05-17 09:03:07 +0000

    updated to merge

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 This file contains deployment related code
 Again i am changing to do second commit
 and i will see how it works.
+
+
+I have added these lines to see difference and then merging
+between branches.
+
